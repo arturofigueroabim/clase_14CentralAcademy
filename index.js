56e0bff021a03416c54e78a91719429990799c51
Arturo Figueroa <arturofigueroa@MacBook-Pro-de-Arturo-5.local>
2019-09-20 21:09:25 -0500

    Index express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require(express);
+const express = require('express');
 const app = express();
 const port = 9000;
 
@@ -7,6 +7,9 @@ app.get('/', function(req,res){
     res.send("Hello World Express!");
 })
 
+app.get('/hello', function(req,res){
+    res.send("Hello World Express! 9000");
+})
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}!`);
 })
