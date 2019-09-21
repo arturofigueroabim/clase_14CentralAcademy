d156471082c09fe139f2c7c8565c93b57dee193c
Arturo Figueroa <arturofigueroa@MacBook-Pro-de-Arturo-5.local>
2019-09-20 21:27:30 -0500

    New Methods

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.get('/', function(req,res){
 app.get('/hello', function(req,res){
     res.send("Hello World Express! 9000");
 })
+
+app.post('/post-express', function(req,res){
+    res.send("Hello World This is a Post! 9000");
+})
+
+app.put('/put-express', function(req,res){
+    res.send("Hello World This is a PUT! 9000");
+})
+
+app.delete('/delete-express', function(req,res){
+    res.send("Hello World This is a Delete! 9000");
+})
+
 app.listen(port, function(){
     console.log(`Example app listening on port ${port}!`);
 })
